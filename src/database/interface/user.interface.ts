3316b3421a60e6393dce5aae8d372befe887786f
Jupaolivera <75101920+Jupaolivera@users.noreply.github.com>
2024-09-29 20:35:05 -0300

    user interface - add name, email, password

--- src/database/interface/user.interface.ts
+++ src/database/interface/user.interface.ts
@@ -2,5 +2,8 @@ import { Types } from 'mongoose';
 import { RolesEnum } from '../../core/enum/roles.enum';
 export interface IUser {
   _id: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
   role: RolesEnum;
 }
